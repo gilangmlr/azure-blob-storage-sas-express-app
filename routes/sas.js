40bed6cd008e430af6c1bd4ad111d3145378a24b
Gilang Gumilar <gilangmlr@gmail.com>
2017-04-30 18:08:06 +0700

    create azure blob service

--- routes/sas.js
+++ routes/sas.js
@@ -1,7 +1,13 @@
+var azure = require('azure-storage');
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+var storageAccount = process.env.AZURE_STORAGE_ACCOUNT || 'ggmlrstor';
+var storageAccessKey = process.env.AZURE_STORAGE_ACCESS_KEY || 't+iOnta9NSPaJgPdee9YOgzzI0wBYemzpjj3Ig8ED0C6wY4S/zNgFfDecV4fDxQLAFrOrGaraQjNXRHQPutYyA==';
+var storageEndpoint = 'https://' + storageAccount + '.blob.core.windows.net/';
+
+var blobService = azure.createBlobService(storageAccount, storageAccessKey);
+
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
