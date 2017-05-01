1af76720c3af83da20d4ea6e48d1e5fb30d29227
Gilang Gumilar <gilangmlr@gmail.com>
2017-05-01 15:28:11 +0700

    remove uploaded filename from blobName

--- routes/sas.js
+++ routes/sas.js
@@ -12,7 +12,7 @@ var blobService = azure.createBlobService(storageAccount, storageAccessKey);
 
 var blobRouter = express.Router();
 
-blobRouter.get('/:container?/:filename?', function(req, res, next) {
+blobRouter.get('/:container?', function(req, res, next) {
   var startDate = new Date();
   var expiryDate = new Date(startDate);
   expiryDate.setMinutes(startDate.getMinutes() + 1);
@@ -37,16 +37,7 @@ blobRouter.get('/:container?/:filename?', function(req, res, next) {
     });
   }
 
-  var blobName;
-  if (req.params.filename) {
-    var split = (req.params.filename).split('.');
-    var name = split.slice(0, split.length - 1).join('.');
-    var ext = split[split.length - 1];
-
-    blobName = name + '-' + uuid.v4() + '.' + ext;
-  } else {
-    blobName = uuid.v4();
-  }
+  var blobName = uuid.v4();
 
   var blobSAS = blobService.generateSharedAccessSignature(storageContainer, blobName, sharedAccessPolicy);
 
