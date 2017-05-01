653c9e17947a6fb160bb848f8a0fdac5a2ac6b11
Gilang Gumilar <gilangmlr@gmail.com>
2017-05-01 19:53:47 +0700

    add put, get, list, delete blob

--- routes/sas.js
+++ routes/sas.js
@@ -12,41 +12,72 @@ var blobService = azure.createBlobService(storageAccount, storageAccessKey);
 
 var blobRouter = express.Router();
 
-blobRouter.get('/:container?', function(req, res, next) {
+function generateSAS(operation, container, filename, permission) {
   var startDate = new Date();
   var expiryDate = new Date(startDate);
   expiryDate.setMinutes(startDate.getMinutes() + 1);
   startDate.setMinutes(startDate.getMinutes() - 1);
 
+  /*
+    azure.BlobUtilities.SharedAccessPermissions.READ + azure.BlobUtilities.SharedAccessPermissions.ADD
+    + azure.BlobUtilities.SharedAccessPermissions.CREATE + azure.BlobUtilities.SharedAccessPermissions.WRITE
+    + azure.BlobUtilities.SharedAccessPermissions.DELETE + azure.BlobUtilities.SharedAccessPermissions.LIST
+  */
+
   var sharedAccessPolicy = {
     AccessPolicy: {
-      Permissions: azure.BlobUtilities.SharedAccessPermissions.READ + azure.BlobUtilities.SharedAccessPermissions.ADD
-        + azure.BlobUtilities.SharedAccessPermissions.CREATE + azure.BlobUtilities.SharedAccessPermissions.WRITE
-        + azure.BlobUtilities.SharedAccessPermissions.DELETE + azure.BlobUtilities.SharedAccessPermissions.LIST,
+      Permissions: permission,
       Start: startDate,
       Expiry: expiryDate
     },
   };
 
   var storageContainer = 'default-container';
-  if (req.params.container) {
-    blobService.createContainerIfNotExists(req.params.container, function(error, result, response){
+  if (container) {
+    blobService.createContainerIfNotExists(container, function(error, result, response){
         if(!error){
-          storageContainer = req.params.container;
+          storageContainer = container;
         }
     });
+    console.log('storageContainer: ' + storageContainer);
   }
 
-  var blobName = uuid.v4();
+  var blobName = filename;
 
   var blobSAS = blobService.generateSharedAccessSignature(storageContainer, blobName, sharedAccessPolicy);
 
+  var sasUrl = storageEndpoint + storageContainer + '/' + blobName + '?' + blobSAS;
+  if (operation == 'list') {
+    sasUrl = storageEndpoint + storageContainer + '?' + blobSAS + '&restype=container&comp=list';
+  }
+
   var result = {
     token: blobSAS,
-    blobURI: storageEndpoint + storageContainer + '/' + blobName,
-    tokenURI: storageEndpoint + storageContainer + '/' + blobName + '?' + blobSAS
+    blobName: blobName,
+    url: sasUrl
   };
 
+  return result;
+}
+
+blobRouter.get('/put/:container?/:filename?', function(req, res, next) {
+  var filename = req.params.filename ? uuid.v4() + '-' + req.params.filename : uuid.v4();
+  var result = generateSAS('put', req.params.container, filename, 'w');
+  res.send(result);
+});
+
+blobRouter.get('/get/:container/:filename', function(req, res, next) {
+  var result = generateSAS('get', req.params.container, req.params.filename, 'r');
+  res.send(result);
+});
+
+blobRouter.get('/list/:container/:filename?', function(req, res, next) {
+  var result = generateSAS('list', req.params.container, undefined, 'l');
+  res.send(result);
+});
+
+blobRouter.get('/delete/:container/:filename', function(req, res, next) {
+  var result = generateSAS('delete', req.params.container, req.params.filename, 'd');
   res.send(result);
 });
 
