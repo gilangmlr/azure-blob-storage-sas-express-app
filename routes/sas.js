6c06c5fbc9aa47ccd698a7a5e76d3e66adf807a6
Gilang Gumilar <gilangmlr@gmail.com>
2017-05-27 01:31:38 +0700

    remove default azure storage key

--- routes/sas.js
+++ routes/sas.js
@@ -4,8 +4,8 @@ var uuid = require('uuid');
 
 var router = express.Router();
 
-var storageAccount = process.env.AZURE_STORAGE_ACCOUNT || 'ggmlrstor';
-var storageAccessKey = process.env.AZURE_STORAGE_ACCESS_KEY || 't+iOnta9NSPaJgPdee9YOgzzI0wBYemzpjj3Ig8ED0C6wY4S/zNgFfDecV4fDxQLAFrOrGaraQjNXRHQPutYyA==';
+var storageAccount = process.env.AZURE_STORAGE_ACCOUNT;
+var storageAccessKey = process.env.AZURE_STORAGE_ACCESS_KEY;
 var storageEndpoint = 'https://' + storageAccount + '.blob.core.windows.net/';
 
 var blobService = azure.createBlobService(storageAccount, storageAccessKey);
